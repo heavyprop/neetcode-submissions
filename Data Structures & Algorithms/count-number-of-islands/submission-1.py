3ca5a655b6daf6d7e1514650117875611a149d66
Daniel <93160574+heavyprop@users.noreply.github.com>
2026-08-21 16:31:26 +0100

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # a set
+        # a dfs or bfs
+
+        searched = set()
+        islands = 0
+        stack = []
+        
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == "1" and (x,y) not in searched:
+                    
+                    stack.append((x, y))
+                    islands += 1
+
+                    while stack:
+                        (x, y) = stack.pop()
+                        searched.add((x, y))
+
+                        if x + 1 < len(grid):
+                            if grid[x + 1][y] == "1" and (x + 1,y) not in searched:
+                                stack.append((x + 1, y))
+
+                        if x - 1 >= 0:
+                            if grid[x - 1][y] == "1" and (x - 1,y) not in searched:
+                                stack.append((x - 1, y))
+
+                        if y + 1 < len(grid[0]) and (x,y + 1) not in searched:
+                            if grid[x][y + 1] == "1":
+                                stack.append((x, y + 1))
+
+                        if y - 1 >= 0:
+                            if grid[x][y - 1] == "1" and (x,y - 1) not in searched:
+                                stack.append((x, y - 1))
+        
+        return islands
+
+# time complexity: outer loops m and n, so O(n x m) with 4 neighbour checks O(4mn) 
+# space complexity: biggest structure we are storing is searched, worse case O(mn)
+
+# so O(m x n) and space O(m x n)
